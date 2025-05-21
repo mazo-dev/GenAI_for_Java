c5ab80ce762cde7a1a72fe17f1cf3254a919a8e4
mazodev <devidas.kassar@yahoo.com>
2025-05-21 12:49:52 -0700

    Initial Version

---
+++ src/main/java/com/dev/App.java
@@ -0,0 +1,10 @@
+package com.dev;
+
+import com.dev.utils.Helper;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+        Helper.greet("Developer");
+    }
+}
---
+++ src/main/java/com/dev/utils/Helper.java
@@ -0,0 +1,7 @@
+package com.dev.utils;
+
+public class Helper {
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+}
