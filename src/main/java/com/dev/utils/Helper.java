784427ea8eeb444a6730e7381eed01468e543bd1
mazodev <devidas.kassar@yahoo.com>
2025-05-21 14:31:19 -0700

    Purposely introduced null pointer exception

--- src/main/java/com/dev/utils/Helper.java
+++ src/main/java/com/dev/utils/Helper.java
@@ -3,5 +3,9 @@ package com.dev.utils;
 public class Helper {
     public static void greet(String name) {
         System.out.println("Hello, " + name + "!");
+        
+        //Purposely introuced null pointer exception
+        String s = null;
+        System.out.println("Display S="+s.toLowerCase());
     }
 }
