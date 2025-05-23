374924752857fd022e149dae298f52e9e1a2c770
mazodev <devidas.kassar@yahoo.com>
2025-05-23 11:42:34 -0700

    Added name=null for testing

--- src/main/java/com/dev/utils/Helper.java
+++ src/main/java/com/dev/utils/Helper.java
@@ -3,9 +3,9 @@ package com.dev.utils;
 public class Helper {
     public static void greet(String name) {
         System.out.println("Hello, " + name + "!");
-        
-        //Purposely introuced null pointer exception
-        String s = null;
-        System.out.println("Display S="+s.toLowerCase());
+
+        //Purposely introduced null pointer exception
+        name = null;
+        System.out.println("Display S="+name.toLowerCase());
     }
 }
