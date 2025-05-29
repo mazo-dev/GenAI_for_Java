6f99636b64af228c6bd932dcc80b8ec00d91f421
mazo-dev <mazodev@gmail.com>
2025-05-29 10:33:23 -0700

    Update Helper.java
    Removed null settings

--- src/main/java/com/dev/utils/Helper.java
+++ src/main/java/com/dev/utils/Helper.java
@@ -3,9 +3,6 @@ package com.dev.utils;
 public class Helper {
     public static void greet(String name) {
         System.out.println("Hello, " + name + "!");
-
-        //Purposely introduced null pointer exception
-        name = null;
         System.out.println("Display S="+name.toLowerCase());
     }
 }
