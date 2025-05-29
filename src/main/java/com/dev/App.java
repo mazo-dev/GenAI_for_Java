880431cdf002795e90216f891160dce3fd8912af
mazo-dev <mazodev@gmail.com>
2025-05-29 10:33:49 -0700

    Update App.java

--- src/main/java/com/dev/App.java
+++ src/main/java/com/dev/App.java
@@ -5,6 +5,6 @@ import com.dev.utils.Helper;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Java!");
-        Helper.greet("Developer");
+        Helper.greet(null);
     }
 }
